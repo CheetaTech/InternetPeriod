cf2d04cc2c7a7312a0162fc697345e85dba995c1
Erkan Güzeler <erkanguzeler@gmail.com>
2016-06-15 23:21:29 +0300

    Usage listesi eklendi.Reklam eklenecek.

--- app/src/main/java/com/mobiledatatimerwidget/InfoFragment.java
+++ app/src/main/java/com/mobiledatatimerwidget/InfoFragment.java
@@ -1,40 +1,81 @@
 package com.mobiledatatimerwidget;
 
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
-import android.view.LayoutInflater;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+
 
 public class InfoFragment extends DialogFragment implements View.OnClickListener {
+
+
+
+    private OnFragmentInteractionListener mListener;
+
+    public InfoFragment() {}
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
-
-
-        View rootView = inflater.inflate(R.layout.info_fragment, container,
-                false);
-
-
-        getDialog().setTitle("Net Guardion Usage");
-
-        /*
-        final WindowManager.LayoutParams params = dialog.getWindow().getAttributes();
-        params.verticalMargin = 0;
-        params.horizontalMargin = 0;
-        params.width = getResources().getDimensionPixelSize(R.dimen.dialog_withhhh);
-        params.height = WindowManager.LayoutParams.WRAP_CONTENT;
-        params.gravity = Gravity.CENTER;
-        */
-        return rootView;
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    public static InfoFragment newInstance() {
+        InfoFragment fragment = new InfoFragment();
+        return fragment;
+    }
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        Log.e("onResume", "onResume");
+        this.getDialog().getWindow().setSoftInputMode(
+                WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
     }
 
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Log.e("OnCreateDialog", "OnCreateDialog");
+        Dialog dialog = new Dialog(getActivity(), android.R.style.Theme_DeviceDefault_Light_Dialog_NoActionBar);
+        View view = getActivity().getLayoutInflater().inflate(R.layout.info_fragment, null);
+        dialog.getWindow().setContentView(view);
+
+        //
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        ((Activity)getContext()).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        dialog.getWindow().setLayout((int) (displayMetrics.widthPixels * 0.8f), (int) (displayMetrics.heightPixels * 0.8f));
+        dialog.setTitle("Internet Period Usage");
+        dialog.setCanceledOnTouchOutside(false);
+
+        ((Button)view.findViewById(R.id.btn_quit)).setOnClickListener(this);
+        return dialog;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Log.e("onViewCreated","onViewCreated");
+    }
     @Override
     public void onClick(View v) {
         switch (v.getId())
         {
-
+            case R.id.btn_quit:
+                    getDialog().dismiss();
+                break;
         }
     }
+
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
 }
+
